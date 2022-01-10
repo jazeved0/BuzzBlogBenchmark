a2cbed6ffce2bf034902712839c4dfb8c56e2fcb
Joseph Azevedo <joseph.az@gatech.edu>
2022-01-10 10:46:32 -0500

    add support for cgroup v2 logs to rAdvisor parser

--- analysis/notebooks/lib/radvisor_parser.py
+++ analysis/notebooks/lib/radvisor_parser.py
@@ -16,7 +16,7 @@ MIN_TARGET_LOG_VERSION="1.3.0"
 
 
 @dataclass
-class IOStats:
+class IOStatsCgroupV1:
     read: int
     write: int
     sync: int
@@ -24,7 +24,7 @@ class IOStats:
 
 
 @dataclass
-class TargetLogEntry:
+class TargetLogEntryCgroupV1:
     read: int
     # PID stats
     pids_current: int
@@ -64,84 +64,111 @@ class TargetLogEntry:
     # Block IO stats
     blkio_time: int
     blkio_sectors: int
-    blkio_service_bytes: IOStats
-    blkio_service_ios: IOStats
-    blkio_service_time: IOStats
-    blkio_queued: IOStats
-    blkio_wait: IOStats
-    blkio_merged: IOStats
-    blkio_throttle_service_bytes: IOStats
-    blkio_throttle_service_ios: IOStats
-    blkio_bfq_service_bytes: IOStats
-    blkio_bfq_service_ios: IOStats
+    blkio_service_bytes: IOStatsCgroupV1
+    blkio_service_ios: IOStatsCgroupV1
+    blkio_service_time: IOStatsCgroupV1
+    blkio_queued: IOStatsCgroupV1
+    blkio_wait: IOStatsCgroupV1
+    blkio_merged: IOStatsCgroupV1
+    blkio_throttle_service_bytes: IOStatsCgroupV1
+    blkio_throttle_service_ios: IOStatsCgroupV1
+    blkio_bfq_service_bytes: IOStatsCgroupV1
+    blkio_bfq_service_ios: IOStatsCgroupV1
 
     @staticmethod
-    def make(row: Dict[str, str]) -> 'TargetLogEntry':
+    def make(row: Dict[str, str]) -> 'TargetLogEntryCgroupV1':
         def i(key: str) -> int:
             """Attempts to parse a cell to an int, or returns 0"""
             if key in row and row[key] is not None and len(row[key]) > 0:
                 return int(row[key])
             return 0
 
-        def io(key_prefix: str) -> IOStats:
+        def io(key_prefix: str) -> IOStatsCgroupV1:
             """Makes an IOStats instance from the key prefix"""
-            return IOStats(
+            return IOStatsCgroupV1(
                 read=i(f"{key_prefix}.read"),
                 write=i(f"{key_prefix}.write"),
                 sync=i(f"{key_prefix}.sync"),
                 asyn=i(f"{key_prefix}.async"))
 
-        return TargetLogEntry(
-            read=int(row["read"]),
-            pids_current=i("pids.current"),
-            pids_max="max" if row["pids.max"] == "max" else i("pids.max"),
-            # CPU stats
-            cpu_usage_total=i("cpu.usage.total"),
-            cpu_usage_system=i("cpu.usage.system"),
-            cpu_usage_user=i("cpu.usage.user"),
-            cpu_usage_percpu=([] if row["cpu.usage.percpu"] == ""
-                else list(map(int, row["cpu.usage.percpu"].split(" ")))),
-            cpu_stat_user=i("cpu.stat.user"),
-            cpu_stat_system=i("cpu.stat.system"),
-            cpu_throttling_periods=i("cpu.throttling.periods"),
-            cpu_throttling_throttled_count=i("cpu.throttling.throttled.count"),
-            cpu_throttling_throttled_time=i("cpu.throttling.throttled.time"),
-            # Memory stats
-            memory_usage_current=i("memory.usage.current"),
-            memory_usage_max=i("memory.usage.max"),
-            memory_limit_hard=i("memory.limit.hard"),
-            memory_limit_soft=i("memory.limit.soft"),
-            memory_failcnt=i("memory_failcnt"),
-            memory_hierarchical_limit_memory=i("memory.hierarchical.limit.memory"),
-            memory_hierarchical_limit_memoryswap=i("memory.hierarchical.limit.memoryswap"),
-            memory_cache=i("memory.cache"),
-            memory_rss_all=i("memory.rss.all"),
-            memory_rss_huge=i("memory.rss.huge"),
-            memory_mapped=i("memory.mapped"),
-            memory_swap=i("memory.swap"),
-            memory_paged_in=i("memory.paged.in"),
-            memory_paged_out=i("memory.paged.out"),
-            memory_fault_total=i("memory.fault.total"),
-            memory_fault_major=i("memory.fault.major"),
-            memory_anon_inactive=i("memory.anon.inactive"),
-            memory_anon_active=i("memory.anon.active"),
-            memory_file_inactive=i("memory.file.inactive"),
-            memory_file_active=i("memory.file.active"),
-            memory_unevictable=i("memory.unevictable"),
-            # Block IO stats
-            blkio_time=i("blkio.time"),
-            blkio_sectors=i("blkio.sectors"),
-            blkio_service_bytes=io("blkio.service.bytes"),
-            blkio_service_ios=io("blkio.service.ios"),
-            blkio_service_time=io("blkio.service.time"),
-            blkio_queued=io("blkio.queued"),
-            blkio_wait=io("blkio.wait"),
-            blkio_merged=io("blkio.merged"),
-            blkio_throttle_service_bytes=io("blkio.throttle.service.bytes"),
-            blkio_throttle_service_ios=io("blkio.throttle.service.ios"),
-            blkio_bfq_service_bytes=io("blkio.bfq.service.bytes"),
-            blkio_bfq_service_ios=io("blkio.bfq.service.ios"))
+        # Every field is an int, except for pids_max, cpu_usage_percpu,
+        # and blkio.* (with the exception of blkio.time and blkio.sectors),
+        # and every field is the same as its column label,
+        # except s/./_/
 
+        io_keys = {k for k in row if (
+            k.startswith("blkio.") and k not in {"blkio.time", "blkio.sectors"})}
+        io_fields = {k.replace('.', '_'): io(v) for k, v in row if k in io_keys}
+
+        int_keys = {k for k in row if (
+            k not in {"pids.max", "cpu.usage.percpu"} and k not in io_keys)}
+        int_fields = {k.replace('.', '_'): i(v) for k, v in row if k in int_keys}
+
+        pids_max = "max" if row["pids.max"] == "max" else i("pids.max")
+        cpu_usage_percpu = []
+        if len(row["cpu.usage.percpu"]) > 0:
+            cpu_usage_percpu = [i(s) for s in row["cpu.usage.percpu"].split(" ")]
+
+        return TargetLogEntryCgroupV1(**io_fields, **int_fields, pids_max=pids_max, cpu_usage_percpu=cpu_usage_percpu)
+
+
+@dataclass
+class TargetLogEntryCgroupV2:
+    read: int
+    # PID stats
+    pids_current: int
+    pids_max: Union[int, str]
+    # CPU stats
+    cpu_stat__usage_usec: int
+    cpu_stat__system_usec: int
+    cpu_stat__user_usec: int
+    cpu_stat__nr_periods: int
+    cpu_stat__nr_throttled: int
+    cpu_stat__throttled_usec: int
+    # Memory stats
+    memory_current: int
+    memory_high: int
+    memory_max: int
+    memory_stat__anon: int
+    memory_stat__file: int
+    memory_stat__kernel_stack: int
+    memory_stat__pagetables: int
+    memory_stat__percpu: int
+    memory_stat__sock: int
+    memory_stat__shmem: int
+    memory_stat__file_mapped: int
+    memory_stat__file_dirty: int
+    memory_stat__file_writeback: int
+    memory_stat__swapcached: int
+    memory_stat__inactive_anon: int
+    memory_stat__active_anon: int
+    memory_stat__inactive_file: int
+    memory_stat__active_file: int
+    memory_stat__unevictable: int
+    memory_stat__pgfault: int
+    memory_stat__pgmajfault: int
+    # Block IO stats
+    io_stat__rbytes: int
+    io_stat__wbytes: int
+    io_stat__rios: int
+    io_stat__wios: int
+    io_stat__dbytes: int
+    io_stat__dios: int
+
+    @staticmethod
+    def make(row: Dict[str, str]) -> 'TargetLogEntryCgroupV2':
+        def i(key: str) -> int:
+            """Attempts to parse a cell to an int, or returns 0"""
+            if key in row and row[key] is not None and len(row[key]) > 0:
+                return int(row[key])
+            return 0
+
+        # Every field is an int, except for pids_max,
+        # and every field is the same as its column label,
+        # except s/./_/ and s/\//__/
+        fields = {k.replace('.', '_').replace('/', '__'): i(v) for k, v in row if k != "pids.max"}
+        pids_max = "max" if row["pids.max"] == "max" else i("pids.max")
+        return TargetLogEntryCgroupV2(**fields, pids_max=pids_max)
 
 @dataclass
 class BufferFlushLogEntry:
@@ -159,11 +186,12 @@ class BufferFlushLogEntry:
             success=row["success"].lower() == 'true')
 
 
-def parse_target_log(lines: Iterable[str]) -> Tuple[Iterable[TargetLogEntry], Dict[str, Any]]:
+def parse_target_log(lines: Iterable[str]) -> Tuple[Union[Iterable[TargetLogEntryCgroupV1], Iterable[TargetLogEntryCgroupV2]], Dict[str, Any]]:
     """
     Loads an output target file from rAdvisor into
-    a lazy iterator of TargetLogEntry in the order of logging,
-    in addition to the metadata dictionary contained at the top of the logfile.
+    a lazy iterator of either TargetLogEntryCgroupV1's or TargetLogEntryCgroupV2's
+    in the order of logging, in addition to the metadata dictionary
+    contained at the top of the logfile.
     """
 
     metadata = {}
@@ -193,12 +221,18 @@ def parse_target_log(lines: Iterable[str]) -> Tuple[Iterable[TargetLogEntry], Di
     if version < MIN_TARGET_LOG_VERSION:
         print(f"Warning: rAdvisor log version '{version}' "
             f"is less than minimum version '{MIN_TARGET_LOG_VERSION}'")
+
+    # Check the collector type
+    collector_type = metadata.get("CollectorType", "cgroup_v1")
     
     csv_reader = csv.DictReader(lines)
     def generator():
         for row in csv_reader:
             try:
-                yield TargetLogEntry.make(row)
+                if collector_type == "cgroup_v1":
+                    yield TargetLogEntryCgroupV1.make(row)
+                elif collector_type == "cgroup_v2":
+                    yield TargetLogEntryCgroupV2.make(row)
             except Exception as e:
                 print(e)
                 print("An error ocurred. continuing...\n")
